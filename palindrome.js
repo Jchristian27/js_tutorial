d928cbe78e4f61203b72d4f78462f2f1383f6d75
Jonathan Christian <christianj707@gmail.com>
2022-10-15 14:31:08 -0700

    Add functions and methods to file

--- palindrome.js
+++ palindrome.js
@@ -1,15 +1,37 @@
-// Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("")
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-// Defines a Phrase object.
+String.prototype.blank = function() {
+  if (this.length === 0 || this === "/\s+/") {
+    return true;
+  }
+}
+
 function Phrase(content) {
   this.content = content;
 
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    let processedContent = this.content.toLowerCase();
-    return processedContent === reverse(processedContent);
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
+
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
   }
 }
